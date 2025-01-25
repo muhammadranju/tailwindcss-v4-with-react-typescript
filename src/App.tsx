b85d51330d6eb18faea1b5df33d1a05c18b5e7f7
Muhammad Ranju <mdranju23@gmail.com>
2025-01-26 00:23:52 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="dark:bg-gray-950 bg-slate-100 text-slate-800 dark:text-slate-100">
+      <div className="container mx-auto   flex flex-col justify-center items-center min-h-screen">
+        <p className="text-2xl font-semibold "> Hello React + TailwindCSS</p>
+        <h1 className="text-9xl font-bold text-center">It's Tailwind CSS V4</h1>
+        <p className="mt-10 font-medium text-4xl">
+          Really cool and impressive also eye catching
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
